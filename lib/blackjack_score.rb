3d61f3287c958259db561dac7de607e01b05c73f
Laura <larmartinez2@gmail.com>
2018-08-20 09:12:19 -0700

    raises an argument error if hand doesn't have at least 2 cards

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,10 +1,26 @@
-# blackjack_score.rb
+require 'pry'
 
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack', 'Ace']
 
 def blackjack_score(hand)
-  sum = 0
+  # verify that input is correct
+  hand.length.times do |i|
+    next if hand[i] == 'Ace' || hand[i] == 'Queen' || hand[i] == 'Jack' || hand[i] == 'King'
+    if hand[i] > 9
+      raise ArgumentError, "invalid card"
+    end
+  end
+
+  if hand.length > 5
+    raise ArgumentError, "you have more than 5 cards"
+  end
+
+  if hand.length < 2
+    raise ArgumentError, "you are missing 1 card"
+  end
+
   # convert face cards into numerical representation
+  sum = 0
   hand.length.times do |i|
     if hand[i] == 'King'
       hand[i] = 10
@@ -33,16 +49,6 @@ def blackjack_score(hand)
     raise ArgumentError, "you went over 21"
   else
     return sum
-  end
-
-  hand.length.times do |i|
-    if hand[i] > 9
-      raise ArgumentError, "invalid card"
-    end
-  end
-
-  if hand.length > 5
-    raise ArgumentError, "you have more than 5 cards"
   end
 
 end
